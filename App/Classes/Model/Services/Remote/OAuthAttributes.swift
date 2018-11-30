e166fb85fbe514a9ab3aedf8512d1c6015177bbc
Andrés Castaño <acastano@gmail.com>
2018-11-30 11:31:55 +0000

    Adding demo credentials

--- App/Classes/Model/Services/Remote/OAuthAttributes.swift
+++ App/Classes/Model/Services/Remote/OAuthAttributes.swift
@@ -4,9 +4,9 @@ enum OAuthAttributes: String {
     case oauth = "OAuth"
     case version = "1.0"
     case signature_method = "HMAC-SHA1"
-    case token = "ENTER HERE THE TOKEN"
-    case consumer_key = "ENTER KEY THE KEY"
-    case consumer_secret = "ENTER HERE THE SECRET"
-    case token_secret = "ENTER HERE THE TOKEN SECRET"
+    case token = "15122320-xn2OopZSl7DhTb12irSr5ukyzoe3G3ASgCANwu2Dc"
+    case consumer_key = "hBghdh0zoPKFs5QS1tL6oRn2Z"
+    case consumer_secret = "KpiCCf8SBljz3lpw7f9VWscA54GzjuGpIuxhfX3A22OTRva9J2"
+    case token_secret = "5Pb8kRZXHjhcbIzmVyRzAyLbas9Ig71TNc0opUahogsfk"
 
 }
